e212fc824e9a4d20d58edf9e1828a54da4ff98a3
nimur40 <nimur40@gmail.com>
2026-06-17 07:04:52 +0600

    Update project

---
+++ myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+class Expense(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    category=models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    amount=models.DecimalField(max_digits=10,decimal_places=2)
+    description=models.TextField(blank=True,null=True)
+    date=models.DateField(auto_now_add=True)
---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    #process1
+    #process2
+    #
+    return  HttpResponse("Hello Django")
